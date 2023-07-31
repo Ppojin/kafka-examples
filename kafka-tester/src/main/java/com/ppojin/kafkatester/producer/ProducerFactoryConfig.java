be757ae8575ec3f120c66a3c2cdacc7935d4b83b
HyojinHwang <hhjin1814@gmail.com>
2023-07-31 09:54:00 +0900

    fix kafka producer

--- kafka-tester/src/main/java/com/ppojin/kafkatester/producer/ProducerFactoryConfig.java
+++ kafka-tester/src/main/java/com/ppojin/kafkatester/producer/ProducerFactoryConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,27 +17,33 @@ import java.util.Map;
 
 @Slf4j
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Configuration
 public class ProducerFactoryConfig {
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory(Map<String, Object> producerConfigs) {
-        return new DefaultKafkaProducerFactory<>(producerConfigs);
+    private final String bootstrapServers;
+
+    public ProducerFactoryConfig(
+            @Value("${kafka.bootstrap_servers:localhost:19092,localhost:29092,localhost:39092}") String bootstrapServers
+    ) {
+        this.bootstrapServers = bootstrapServers;
     }
 
-    @Bean
-    public Map<String, Object> producerConfigs(
-            @Value("${kafka.bootstrap_servers:kafka0:9092,kafka1:9093,kafka2:9094}") String bootstrapServers
-    ) {
+    public Map<String, Object> producerConfigs() {
         return Map.ofEntries(
+                Map.entry(ProducerConfig.CLIENT_ID_CONFIG, "customProducer"),
                 Map.entry(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers),
-                Map.entry(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class),
+                Map.entry(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class),
                 Map.entry(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
         );
     }
 
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
     @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 
 }
